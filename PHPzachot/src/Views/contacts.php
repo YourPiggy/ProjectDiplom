59355e7334281d5c41b07c7ad9817ad41d74453e
YourPiggy <48221686+YourPiggy@users.noreply.github.com>
2019-07-19 23:43:54 +0300

    Add files via upload

---
+++ PHPzachot/src/Views/contacts.php
@@ -0,0 +1,15 @@
+<div class="slc">
+  <div class="contacts">
+  <h1>Контакты</h1>
+  <p><strong>191028, город Санкт-Петербург, улица Пестеля, дом 27, офис 27</strong></p>
+  <p><strong>телефон/факс: 8 (812) 648-29-48, 272-47-81</strong></p>
+  <p><strong>эл. почта: info@i-slc.ru</strong></p>
+  </div>
+  <div class="map">
+    <iframe src="http://maps.google.ru/maps?f=q&amp;source=s_q&amp;hl=ru&amp;geocode=&amp;q=%D0%B3%D0%BE%D1%80%D0%BE%D0%B4+%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3,+%D1%83%D0%BB%D0%B8%D1%86%D0%B0+%D0%9F%D0%B5%D1%81%D1%82%D0%B5%D0%BB%D1%8F+%D0%B4%D0%BE%D0%BC+27&amp;aq=1&amp;sll=55.354135,40.297852&amp;sspn=17.773497,53.525391&amp;ie=UTF8&amp;hq=&amp;hnear=%D1%83%D0%BB.+%D0%9F%D0%B5%D1%81%D1%82%D0%B5%D0%BB%D1%8F,+27,+%D0%9B%D0%B8%D1%82%D0%B5%D0%B9%D0%BD%D1%8B%D0%B9,+%D0%B3%D0%BE%D1%80%D0%BE%D0%B4+%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3,+%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9+%D0%90%D0%B4%D0%BC.+%D1%80%D0%B0%D0%B9%D0%BE%D0%BD,+191028&amp;ll=59.942707,30.349646&amp;spn=0.005159,0.013733&amp;z=16&amp;output=embed" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="640" height="480"></iframe>
+  </div>
+  <div class="foot2">
+        <p>Тел.: 8(812)648-29-48</p>
+        <p>© 2019 «Smart Lawyers Company».</p>
+  </div>
+</div>
